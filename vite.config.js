03d882e032faf84d09d046150d58244b32c86aa4
Pam <prati.ma@hotmail.com>
2024-09-08 19:55:29 +0200

    fix Case

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/todoList/",
+  base: "/TodoList/",
   plugins: [react(), VitePWA()],
 });
