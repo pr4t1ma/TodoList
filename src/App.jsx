71263c4cfcf809b03a637c61ded804b31dc3e652
Pam <prati.ma@hotmail.com>
2024-09-08 14:08:46 +0200

    Create To-Do list feature with state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,67 @@
+import { Filter, Menu } from "lucide-react";
 import "./App.css";
+import TodoItem from "./TodoItem";
+import { useEffect, useState } from "react";
+const items = [
+  { id: 1, name: "orange", completed: false },
+  { id: 2, name: "apple", completed: true },
+  { id: 3, name: "banana", completed: false },
+  { id: 4, name: "grape", completed: true },
+  { id: 5, name: "mango", completed: false },
+  { id: 6, name: "pear", completed: true },
+  { id: 7, name: "pineapple", completed: false },
+];
 
 function App() {
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    const storeItems = localStorage.getItem("todoItems");
+    if (storeItems) {
+      setItems(JSON.parse(storeItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (items) localStorage.setItem("todoItems", JSON.stringify(items));
+  }, [items]);
+
   return (
-    <>
-      <h1>Todo List</h1>
-      <form action="">
-        <input type="text" placeholder="type here" />
+    <div className="w-1/2 container mx-auto my-20">
+      <div className="flex justify-between between mb-10">
+        <Menu className="red-500" color="black" size={24} />
+        <h1>Todo List</h1>
+        <Filter className="red-500" color="black" size={24} />
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          setItems([...items, { name: e.target[0].value }]);
+        }}
+        className="border-1 mb-4 flex border-black bg-red-500"
+      >
+        <input
+          name="text"
+          className="border py-2 flex-1 p-4"
+          type="text"
+          required
+          placeholder="New Item"
+        />
+        <button className="bg-blue-700 text-gray-200 px-5 py-2">Add</button>
       </form>
-    </>
+      <div className="itemlist">
+        {items?.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              text={item.name}
+              completed={item.completed}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
---
+++ src/TodoItem.jsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import { Ellipsis } from "lucide-react";
+import { useState } from "react";
+
+const TodoItem = ({ text, completed }) => {
+  const [isComplete, setIsComplete] = useState(completed);
+  return (
+    <div className="flex gap-4  border border-gray-200 py-2 px-4">
+      <input
+        onChange={(e) => setIsComplete(e.target.checked)}
+        type="checkbox"
+        checked={isComplete}
+      />
+      <p className={classNames("flex-1", isComplete ? "line-through" : "")}>
+        {text}
+      </p>
+      <button>
+        <Ellipsis className="red-500" color="black" size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
