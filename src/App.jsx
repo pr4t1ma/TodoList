9b7ed564c7bd44a2311f326ac4af71527c094644
Pam <prati.ma@hotmail.com>
2024-09-08 11:02:18 +0200

    Set up react vite with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Todo List</h1>
+      <form action="">
+        <input type="text" placeholder="type here" />
+      </form>
+    </>
+  );
+}
+
+export default App;
