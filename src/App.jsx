f748d32de6a24e8588d468f31c2586ffd1aa885a
Pam <prati.ma@hotmail.com>
2024-09-08 16:54:54 +0200

    Add form reset functionality on submit

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,10 @@ import { Filter, Menu } from "lucide-react";
 import "./App.css";
 import TodoItem from "./TodoItem";
 import { useEffect, useState } from "react";
-const items = [
-  { id: 1, name: "orange", completed: false },
-  { id: 2, name: "apple", completed: true },
-  { id: 3, name: "banana", completed: false },
-  { id: 4, name: "grape", completed: true },
-  { id: 5, name: "mango", completed: false },
-  { id: 6, name: "pear", completed: true },
-  { id: 7, name: "pineapple", completed: false },
-];
 
 function App() {
   const [items, setItems] = useState();
-
+  const [newItem, setNewItem] = useState("");
   useEffect(() => {
     const storeItems = localStorage.getItem("todoItems");
     if (storeItems) {
@@ -36,8 +27,8 @@ function App() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-
           setItems([...items, { name: e.target[0].value, id: Math.random() }]);
+          setNewItem("");
         }}
         className="border-1 mb-4 flex border-black bg-red-500"
       >
@@ -47,6 +38,8 @@ function App() {
           type="text"
           required
           placeholder="New Item"
+          onChange={(e) => setNewItem(e.target.value)}
+          value={newItem}
         />
         <button className="bg-blue-700 text-gray-200 px-5 py-2">Add</button>
       </form>
